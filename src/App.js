dbfd54cfd257a85f6c959d600b479b55cdd5b159
kevinsa <kevsae@hotmail.com>
2022-01-14 12:26:42 -0500

    Moved routes into App component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCoffee } from '@fortawesome/free-solid-svg-icons';
-import { Outlet } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './context/Auth';
 import TopNavBar from './components/topnavbar/TopNavBar';
 import SideBar from './components/sidebar/SideBar';
+import Home from './containers/home/Home';
+import Profile from './containers/profile/Profile';
+import Login from './containers/login/Login';
 import './App.css';
 
 
@@ -16,7 +17,12 @@ function App() {
           <TopNavBar />
           <div className="content">
             <div className="container">
-              <Outlet />
+              <Routes>
+                <Route path="/home" elemement={<Home />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/profile" element={<Profile />} />
+                <Route path="/" element={<Home />} />
+              </Routes>
             </div>
           </div>
         </div>
